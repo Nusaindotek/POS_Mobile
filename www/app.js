2d1266be21328f61a583109f8d13bd86972a4f84
Nusaindotek <nusaindoteknologi@gmail.com>
2026-07-13 17:52:13 +0700

    Update app.js

--- www/app.js
+++ www/app.js
@@ -3,26 +3,19 @@ const request = indexedDB.open("NUSAPOS_DB", 1);
 
 request.onupgradeneeded = (e) => {
     db = e.target.result;
-    if (!db.objectStoreNames.contains('transaksi')) {
-        db.createObjectStore('transaksi', { keyPath: 'id', autoIncrement: true });
-    }
+    if (!db.objectStoreNames.contains('transaksi')) db.createObjectStore('transaksi', { keyPath: 'id', autoIncrement: true });
 };
-
 request.onsuccess = (e) => {
     db = e.target.result;
-    // Event ini memberi tahu halaman lain bahwa DB sudah siap digunakan
-    window.dispatchEvent(new Event('db-ready'));
+    window.dispatchEvent(new Event('db-ready')); // Sinyal database siap
 };
 
-window.saveTransaction = async (data) => {
-    return new Promise((resolve) => {
-        const tx = db.transaction('transaksi', 'readwrite');
-        tx.objectStore('transaksi').add(data);
-        tx.oncomplete = () => resolve();
-    });
+window.saveTransaction = (data) => {
+    const tx = db.transaction('transaksi', 'readwrite');
+    tx.objectStore('transaksi').add(data);
 };
 
-window.getAllData = async (store) => {
+window.getAllData = (store) => {
     return new Promise((resolve) => {
         const req = db.transaction(store, "readonly").objectStore(store).getAll();
         req.onsuccess = () => resolve(req.result);
