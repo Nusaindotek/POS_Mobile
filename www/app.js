69fa205ec95538d8c6ce745b514676f9245e3af1
Nusaindotek <nusaindoteknologi@gmail.com>
2026-07-13 18:30:54 +0700

    Update app.js

--- www/app.js
+++ www/app.js
@@ -1,23 +1,9 @@
-let db;
-const request = indexedDB.open("NUSAPOS_DB", 1);
+// app.js - Fungsi untuk semua halaman
+function simpanData(key, data) {
+    localStorage.setItem(key, JSON.stringify(data));
+    alert('Data berhasil disimpan!');
+}
 
-request.onupgradeneeded = (e) => {
-    db = e.target.result;
-    if (!db.objectStoreNames.contains('transaksi')) db.createObjectStore('transaksi', { keyPath: 'id', autoIncrement: true });
-};
-request.onsuccess = (e) => {
-    db = e.target.result;
-    window.dispatchEvent(new Event('db-ready')); // Sinyal database siap
-};
-
-window.saveTransaction = (data) => {
-    const tx = db.transaction('transaksi', 'readwrite');
-    tx.objectStore('transaksi').add(data);
-};
-
-window.getAllData = (store) => {
-    return new Promise((resolve) => {
-        const req = db.transaction(store, "readonly").objectStore(store).getAll();
-        req.onsuccess = () => resolve(req.result);
-    });
-};
+function ambilData(key) {
+    return JSON.parse(localStorage.getItem(key));
+}
