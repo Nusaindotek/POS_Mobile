0b084e5e66e6048c62ad59f263ed896e22f1c223
Nusaindotek <nusaindoteknologi@gmail.com>
2026-07-14 08:21:05 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'nusapos-v1';
+const urlsToCache = [
+  'dashboard.html',
+  'catalog.html', 
+  'report.html',
+  'setting.html',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
